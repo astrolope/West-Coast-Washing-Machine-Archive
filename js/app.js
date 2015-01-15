8387f7f6294a29ef50abbe2ca8c4c8333fdff7df
astrolope <astrolope@gmail.com>
2015-01-15 15:16:24 -0800

    hmm

--- js/app.js
+++ js/app.js
@@ -97,4 +97,26 @@ function getSearchJSON()
             // Do the actual search
             doSearch(e);
         });
+      
+    //SCROLL TO TOP FUNCTION
+    //Show scroll to top when passes offset variable. 
+    var offset = 300;
+    var duration = 350;
+  
+  
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
+    
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    });
+  
     });
